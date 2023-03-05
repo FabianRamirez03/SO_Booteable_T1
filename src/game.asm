289b30db59e2a2764e7694ce133972f58efbd332
FabianRamirez03 <ramirezfabian003@gmail.com>
2023-03-04 19:38:58 -0600

    Estructura inicial de la pantalla de bienvenida

--- src/game.asm
+++ src/game.asm
@@ -21,6 +21,8 @@ menuWelc  dw '             Bienvenido             ', 0h
 menuDeco2 dw '------------------------------------', 0h
 menuSpace dw '   Presione ESPACIO para continuar  ', 0h
 
+textColor     dw 09h
+
 
 ;Inicia el juego
 startProgram:
@@ -35,6 +37,25 @@ initDisplay:
     mov al, 13h     ;  320x200 con 256 colores
     int 10h         ;  Interrupcion
     ret
+
+
+
+
+;  Ciclo del menu de bienvenida -----------------------------------------------------------------------
+
+menuLoop:                           ; Ciclo principal del menu de bienvenida
+    mov     ah, 00h                 ; Activa obtener el tiempo de la computadora
+    int     1ah                     ; Ejecutar interrupcion
+
+    cmp     dl, [time]              ; Compara el tiempo actual con el tiempo anterior
+    je      menuLoop                ; Si son iguales vuelve a calcular el ciclo
+    mov     [time], dl              ; Sino, almacena el nuevo tiempo
+  
+    call    checkPlayerMenuAction   ; Llama la funcion encargada de verificar teclas en el menu principal
+
+    call    drawTextMenu            ; Llama a la funcion encargada de escribir texto del menu
+
+    jmp     menuLoop                ; Salta al incio de la funcion
 
 
 ;  Limpia el contenido que haya en la pantalla ----------------------------------------------------------
@@ -62,16 +83,99 @@ clearScreenAux2:
     ret                             ; Sino, Retornar
 
 
-menuLoop:                           ; Ciclo principal del menu
-    mov     ah, 00h                 ; Activa obtener el tiempo de la computadora
-    int     1ah                     ; Ejecutar interrupcion
+; Comprueba si el usuario presiona espacio en el menu de bienvenida ------------------------------------------
 
-    cmp     dl, [time]              ; Compara el tiempo actual con el tiempo anterior
-    je      menuLoop                ; Si son iguales vuelve a calcular el ciclo
-    mov     [time], dl              ; Sino, almacena el nuevo tiempo
-  
-    call    checkPlayerMenuAction   ; Llama la funcion encargada de verificar teclas en el menu principal
+checkPlayerMenuAction:              ; Funcion encargada de verificar la tecla presionada en el menu
+    mov     ah, 01h                 ; Consigue el estado del teclado
+    int     16h                     ; Ejecutar interrupcion
+    jz      exitRoutine             ; Si no se esta presionando nada, salta a salir
+    
+    mov     ah, 00h                 ; Lectura de tecla
+    int     16h                     ; Ejecutar interrupcion
 
-    call    drawTextMenu            ; Llama a la funcion encargada de escribir texto del menu
+    cmp     ah, 39h                 ; Si la tecla presionada es Espacio
+    je      startGame               ; Inicia el juego
 
-    jmp     menuLoop                ; Salta al incio de la funcion
+    ret
+
+
+; Dibuja el textp el texto en el menu de bienvenida ----------------------------------------------------------
+
+drawTextMenu:                       ; Funcion encargada de escribir los textos del menu de bienvenida
+    mov     bx, [textColor]         ; Mueve a bx el color del texto
+    inc     bx                      ; Incrementa bx
+    mov     [textColor], bx         ; Almacena el nuevo bx al color del texto
+
+    ; Elemento decorativo de la pantalla de bienvenida
+    mov     bx, menuDeco1           ; Mueve a bx el puntero del primer texto
+    mov     dh, 07h                 ; Mueve a dh un 7
+    mov     dl, 02h                 ; Mueve a dl un 2
+    call    drawText                ; Llama a la funcion encargada de escribir texto
+
+    ; Titulo de la pantalla de bienvenida
+    mov     bx, menuTitle           ; Mueve a bx el puntero del segundo texto
+    inc     dh                      ; Incrementa dh
+    inc     dh                      ; Incrementa dh
+    mov     dl, 02h                 ; Mueve a dl un 2
+    call    drawText                ; Llama a la funcion encargada de escribir texto
+
+    ; Texto de bienvenida de la pantalla de bienvenida
+    mov     bx, menuWelc            ; Mueve a bx el puntero del tercer texto
+    inc     dh                      ; Incrementa dh
+    mov     dl, 02h                 ; Mueve a dl un 2
+    call    drawText                ; Llama a la funcion encargada de escribir texto
+
+    ; Elemento decorativo de la pantalla de bienvenida
+    mov     bx, menuDeco2           ; Mueve a bx el puntero del cuarto texto
+    inc     dh                      ; Incrementa dh
+    inc     dh                      ; Incrementa dh
+    mov     dl, 02h                 ; Mueve a dl un 2
+    call    drawText                ; Llama a la funcion encargada de escribir texto
+
+    ; Texto para indicarle al usuario que presione espacio de la pantalla de bienvenida
+    mov     bx, menuSpace           ; Mueve a bx el puntero del quinta texto
+    inc     dh                      ; Incrementa dh
+    inc     dh                      ; Incrementa dh
+    inc     dh                      ; Incrementa dh
+    mov     dl, 02h                 ; Mueve a dl un 2
+    call    drawText                ; Llama a la funcion encargada de escribir texto
+
+    ret
+
+; Funcion encargada de dibujar el texto en pantalla --------------------------------------------------
+
+drawText:                           ; Funcion encargada de dibujar texto
+    cmp     byte [bx],0             ; Compara el byte que contiene bx con 0
+    jz      finishDraw              ; Si es igual a cero, salta a la funcion de salida. Es un ret
+    jmp     drawChar                ; Sino salta a dibujar un caracter
+
+drawChar:                           ; Funcion encargada de dibujar un caracter
+    push    bx                      ; Hace un push de bx
+    mov     ah, 02h                 ; Mueve a ah un 2
+    mov     bh, 00h                 ; Mueve a bh un 0
+    int     10h                     ; Ejecutar interrupcion
+    pop     bx                      ; Hace pop a bx
+
+    push    bx                      ; Hace push a bx
+    mov     al, [bx]                ; Mueve el contenido de bx a al
+    mov     ah, 0ah                 ; Mueve a ah un 10
+    mov     bh, 00h                 ; Mueve a bh un 0
+    mov     bl, [textColor]         ; Mueve el color de texto a bl
+    mov     cx, 01h                 ; Mueve a cx un 1
+    int     10h                     ; Ejecutar interrupcion
+    pop     bx                      ; Hace pop a bx
+
+    inc     bx                      ; Incrementa bx
+    inc     dl                      ; Incrementa dl
+    jmp     drawText                ; Salta a la funcion de dibujar texto
+
+finishDraw:                         ; Funcion de salida de texto
+    ret                             ; Retornar
+
+
+
+
+; Funcion encargada de retornar --------------------------------------------------
+
+exitRoutine:                        ; Funcion encargada de retornar
+    ret                             ; Retornar
