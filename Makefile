4beb4ac47aa8598d848cb3f642b4eef4bf86129f
Wajib Zaglul <wajo@estudiantec.cr>
2023-03-13 09:14:28 -0600

    [fix] bootloader floppy disk

--- Makefile
+++ Makefile
@@ -19,18 +19,5 @@ debugServer:
 debugClient:
 	gdb -ex "target remote localhost:1234" -ex "set architecture i8086" -ex "set disassembly-flavor intel" -ex "b *0x7C00" -ex "b *0x8000" -ex "c"
 
-hardware:
-	nasm -fbin src/boot.asm -o src/boot.bin
-	nasm -fbin src/game.asm -o src/game.bin
-	cat src/boot.bin src/game.bin > out/program.bin
-	sudo umount /dev/sdb
-	sudo dd if=out/program.bin of=/dev/sdb bs=5632 count=1
-
-hardwareU:
-	nasm -fbin src/boot.asm -o src/boot.bin
-	nasm -fbin src/game.asm -o src/game.bin
-	cat src/boot.bin src/game.bin > out/program.bin
-	sudo dd if=out/program.bin of=/dev/sdb bs=5632 count=1
-
 clean:
 	rm src/*.bin
--- src/boot.asm
+++ src/boot.asm
@@ -1,43 +1,83 @@
+; Boot sector for loading an assembly program from USB
+
 org 0x7C00
-bits 16
+
+%define SECTORS_TO_READ 0xA ; Number of sectors to read from disk
+
+jmp short start
+nop
+
+; Boot Parameter Block
+OEMLabel db "Sistema "	; Disk label
+BytesPerSector dw 512 ; Bytes per sector
+SectorsPerCluster db 1 ; Sectors per cluster
+ReservedSectors dw 1 ; Reserved sectors for boot record
+NumFATs db 2 ; Number of copies of the FAT
+RootDirEntries dw 224 ; Number of entries in root directory
+TotalSectors dw 2880 ; Total number of sectors
+MediaByte db 0xF0 ; Media descriptor byte
+SectorsPerFAT dw 9 ; Sectors per FAT
+SectorsPerTrack dw 18 ; Sectors per track
+NumHeads dw 2 ; Number of heads
+HiddenSectors dd 0 ; Number of hidden sectors
+LargeSectors dd 0 ; Number of LBA sectors
+DriveNumber db 0 ; Drive number
+Signature db 0x41 ; Drive signature
+VolumeID dd 0 ; Volume ID
+VolumeLabel db "Operativos " ; Volume label (11 characters)
+FileSystem db "FAT12 " ; File system type
 
 start:
-    ; Initialize stack pointer
-    xor ax, ax
-    mov ss, ax
-    mov sp, 0x7c00
-    
-    ; Load boot sector into memory
-    mov bx, 0x8000 ; Destination address
-    mov dl, 0x80   ; USB drive number (may need to be changed)
-    mov dh, 0      ; Head number
-    mov cx, 1      ; Sector number
-    mov ah, 0x02   ; BIOS read sector function
-    mov al, 1      ; Number of sectors to read
-    int 0x13       ; Call BIOS disk I/O function
-    jc boot_error  ; Jump to error handling if read fails
+; Initialize registers
+cli
+xor ax, ax
+mov ds, ax
+mov ss, ax
+mov es, ax
+mov fs, ax
+mov gs, ax
+mov sp, 0x6EF0 ; Set stack pointer
+sti
 
-    ; Jump to start of loaded program
-    jmp 0x8000
 
-boot_error:
-    ; Display error message and halt
-    mov ah, 0x0E ; BIOS print function
-    mov al, 'E'  ; Error code
-    int 0x10
-    mov al, 'r'
-    int 0x10
-    mov al, 'r'
-    int 0x10
-    mov al, 'o'
-    int 0x10
-    mov al, 'r'
-    int 0x10
-    mov al, '!'
-    int 0x10
-    hlt
-    jmp $
+; Reset disk system
+mov ah, 0
+int 0x13     ; BIOS disk I/O
+jc errorpart
 
-; Pad boot sector to 512 bytes
-times 510-($-$$) db 0
-dw 0xAA55     ; Boot sector signature
+; Read from disk and write to memory
+mov bx, 0x8000         ; Address to write the program to
+mov al, SECTORS_TO_READ ; Number of sectors to read
+mov ch, 0              ; Cylinder
+mov dh, 0              ; Head
+mov cl, 2              ; Sector
+mov ah, 2              ; AH=2: read disk sectors
+int 0x13               ; BIOS disk I/O
+jmp 0x8000
+jc errorpart           ; Jump to error handling if the BIOS returns an error
+
+
+
+
+
+errorpart:
+; Print error message
+mov si, errormsg
+mov bh, 0x00 ; Page 0
+mov bl, 0x07 ; Text attribute
+mov ah, 0x0E ; AH=0x0E: BIOS print character
+.printloop:
+lodsb ; Load a byte from SI into AL
+sub al, 0 ; Subtract 0 from AL to check for null terminator
+jz end ; Jump to end if null terminator is found
+int 0x10 ; Print character in AL using BIOS video services
+jmp .printloop
+
+end:
+jmp $ ; Infinite loop
+
+errormsg db "Error booting...", 0
+
+times 510-($-$$) db 0 ; Pad with zeros until byte 510
+db 0x55 ; Signature byte 1
+db 0xAA ; Signature byte 2
